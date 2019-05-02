e3bf7cce1e8d3218e58492d99cbca153aa0d1569
pooja rathore <pooja.rathore@amdocs.com>
2019-05-02 18:00:14 +0530

    updated versions

--- client/src/App.js
+++ client/src/App.js
@@ -72,7 +72,7 @@ class App extends Component {
   // our first get method that uses our backend api to 
   // fetch data from our data base
   getDataFromDb = () => {
-    fetch("http://localhost:3001/api/getData")
+    fetch(window.location.origin+"/api/getData")
       .then(data => data.json())
       .then(res => this.setState({ data: res.data }));
   };
